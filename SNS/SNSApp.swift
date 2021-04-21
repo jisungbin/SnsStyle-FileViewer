d56114bb1191077fdfa89b8a860612e42201b15d
jisungbin <ji@sungb.in>
2021-04-21 21:58:44 +0900

    Initial Commit

---
+++ SNS/SNSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SNSApp.swift
+//  SNS
+//
+//  Created by Ji Sungbin on 2021/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SNSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
